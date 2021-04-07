afe1725508e8f1c589ef019f6a6a40353db8d0e2
Sushanth Bhat <sushanthbhat15@gmail.com>
2021-04-07 19:31:51 +0530

    added more functionality

--- To Do List/app.js
+++ To Do List/app.js
@@ -1,15 +1,35 @@
 let plusIcon = document.querySelector(".fa-plus-circle");
-let delIcon = document.querySelector(".delIcons");
+let delIcons = document.querySelectorAll(".delIcons");
 let addTodoInput = document.querySelector("input");
 let listOfTodos = [];
-let listOfLis;
+let listOfLis = document.querySelectorAll(".todos");
+let spans = document.querySelectorAll("span");
 
-plusIcon.addEventListener("click", function(){
-    if(addTodoInput.style.display!="none"){
+plusIcon.addEventListener("click", function () {
+    if (addTodoInput.style.display != "none") {
         addTodoInput.style.display = "none";
     }
-    else{
-        addTodoInput.style.display="inline-block";
+    else {
+        addTodoInput.style.display = "inline-block";
     }
-    
 });
+
+for (let todo of listOfLis) {
+    listOfTodos.push(todo.textContent);
+}
+
+for (let ind = 0; ind < delIcons.length; ind++) {
+    let delIcon = delIcons[ind];
+    delIcon.addEventListener("click", function () {
+        listOfLis[ind + 1].remove();
+        listOfTodos.splice(ind, 1);
+        console.log(listOfTodos);
+    });
+}
+
+for (let ind = 0; ind < delIcons.length; ind++) {
+    let sp = spans[ind];
+    sp.addEventListener("click", function () {
+        sp.classList.toggle("completed");
+    });
+}
